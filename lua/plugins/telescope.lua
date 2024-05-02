89868c44407a46ea30e1695970513a695023bec6
Manuel Vallejo Sabadell <manuel.v.s1999@gmail.com>
2024-05-02 20:12:55 +0200

    Skill issue & Telescope rework

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,12 +1,23 @@
 return{
   'nvim-telescope/telescope.nvim',
-  tag = '0.1.5',
+  branch = '0.1.6',
   dependencies = {'nvim-lua/plenary.nvim'},
   config = function()
     local builtin = require("telescope.builtin")
     vim.keymap.set('n', '<leader>tff', builtin.find_files, {})
-    vim.keymap.set('n', '<leader>tfg', builtin.live_grep, {})
+    vim.keymap.set('n', '<leader>tfg', builtin.git_files, {})
+    vim.keymap.set('n', '<leader>tgs', function ()
+      local word = vim.fn.expand("<cword>")
+      builtin.grep_string({ search = word })
+    end)
+    vim.keymap.set('n', '<leader>tgS', function ()
+      local word = vim.fn.expand("<cWORD>")
+      builtin.grep_string({ search = word })
+    end)
+    vim.keymap.set('n', '<leader>tGs', function ()
+      builtin.grep_string({ search = vim.fn.input("Grep > ") })
+    end)
     vim.keymap.set('n', '<leader>tfb', builtin.buffers, {})
-    vim.keymap.set('n', '<leader>tfh', builtin.help_tags, {})
+    vim.keymap.set('n', '<leader>tht', builtin.help_tags, {})
   end,
 }
