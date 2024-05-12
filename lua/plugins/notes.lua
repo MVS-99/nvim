932cac6f5c97d6e884de36b0d370f52fe033506a
Manuel Vallejo Sabadell <manuel.v.s1999@gmail.com>
2024-05-12 19:48:42 +0200

    adding markdown preview!

--- lua/plugins/notes.lua
+++ lua/plugins/notes.lua
@@ -1,15 +1,22 @@
-return{
-    {"vhyrro/luarocks.nvim",
-      priority = 1001,
-      config = true,
-      opts = {
-        rocks = { "magick" },
-      },
+return
+{
+  {"vhyrro/luarocks.nvim",
+    priority = 1001,
+    config = true,
+    opts = {
+      rocks = { "magick" },
     },
-    {"nvim-neorg/neorg",
-      dependencies = { "luarocks.nvim" },
-      lazy = false,
-      version = "*", -- Latest stable release
-      config = true
-    }
+  },
+  {"nvim-neorg/neorg",
+    dependencies = { "luarocks.nvim" },
+    lazy = false,
+    version = "*", -- Latest stable release
+    config = true
+  },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function() vim.fn["mkdp#util#install"]() end,
+  }
 }
