e563b35d815711fad09ef3d670385c2e570ea0ec
Manuel Vallejo Sabadell <manuel.v.s1999@gmail.com>
2024-08-12 10:33:45 +0200

    New modifications & L/W diff

--- init.lua
+++ init.lua
@@ -55,18 +55,4 @@ vim.opt.updatetime = 50
 
 vim.g.mapleader = " "
 
--- Clipboard hotfix (chapuza en producción)
-vim.cmd([[ let g:clipboard = {
-    \ 'name': 'WslClipboard',
-    \ 'copy': {
-        \ '+': 'clip.exe',
-        \ '*': 'clip.exe',
-    \ },
-    \ 'paste': {
-        \ '+': 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-        \ '*': 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-    \ },
-    \ 'cache_enabled': 0,
-\ }]])
-
 require("lazy-setup")
