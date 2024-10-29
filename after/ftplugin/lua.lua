755aa96a5f99d36819d34ec2b9d03e2cfd0fb9dd
Manuel Vallejo Sabadell <manuel.v.s1999@gmail.com>
2024-10-29 10:15:52 +0100

    Improvements

--- after/ftplugin/lua.lua
+++ after/ftplugin/lua.lua
@@ -1,2 +1,4 @@
 -- Configurations just for lua files
-vim.cmd("setlocal tabstop=2 shiftwidth=2")
+vim.opt.expandtab = true  -- Use spaces instead of tabs
+vim.opt.shiftwidth = 3  -- Number of spaces for each indentation level
+vim.opt.tabstop = 3 -- Number of spaces per Tab
